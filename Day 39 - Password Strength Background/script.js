af894c8067c5edbd1dbd0cd40721cc059943f1a5
Aditi Thakkar <85191076+git-aditi@users.noreply.github.com>
2023-11-27 22:32:11 -0500

    Create script.js

---
+++ Day 39 - Password Strength Background/script.js
@@ -0,0 +1,10 @@
+const password = document.getElementById("password");
+const bacground = document.getElementById("background");
+
+password.addEventListener("input", (e) => {
+  const input = e.target.value;
+  const length = input.length;
+
+  const blurValue = 20 - length * 2;
+  background.style.filter = `blur(${blurValue}px)`;
+});
